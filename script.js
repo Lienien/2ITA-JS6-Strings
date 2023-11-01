ca5d25a2002682dad19111f47edf846dbdfd478e
Lienien <116272320+Lienien@users.noreply.github.com>
2023-11-01 08:21:30 +0100

    bing bada boom

---
+++ script.js
@@ -0,0 +1,13 @@
+//Oppgave 1
+/*
+if (var1.length < 1) {
+    console.log("Teksten er tom");
+} else {
+    console.log("Teksten er ikke tom");
+}
+*/
+//Oppgave 2
+
+const string = "Røyken Videregående Skole"
+
+console.log(string.split(" "));
